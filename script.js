652e65b9d0e2e4d361a368510f6fc9093ddf0098
Easy Learn Axis <128692749+dnam3124@users.noreply.github.com>
2023-10-09 01:09:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
-// script.js
-// Mảng các sản phẩm
-const products = [
-    { id: 1, name: "Sản phẩm 1", price: 99.99 },
-    { id: 2, name: "Sản phẩm 2", price: 49.99 },
-    // Thêm các sản phẩm khác ở đây
-];
-
-// Hàm để thêm sản phẩm vào danh sách
-function addProductToUI(product) {
-    const productList = document.getElementById("product-list");
-    const productCard = document.createElement("div");
-    productCard.classList.add("col-md-4", "mb-4");
-
-    productCard.innerHTML = `
-        <div class="card">
-            <img src="product${product.id}.jpg" class="card-img-top" alt="${product.name}">
-            <div class="card-body">
-                <h5 class="card-title">${product.name}</h5>
-                <p class="card-text">Giá: $${product.price.toFixed(2)}</p>
-                <button class="btn btn-primary" onclick="addToCart(${product.id})">Thêm vào giỏ hàng</button>
-            </div>
-        </div>
-    `;
-
-    productList.appendChild(productCard);
-}
-
-// Hàm để thêm sản phẩm vào giỏ hàng
-function addToCart(productId) {
-    const product = products.find(p => p.id === productId);
-    // Thực hiện logic thêm sản phẩm vào giỏ hàng ở đây
-    alert(`Đã thêm sản phẩm ${product.name} vào giỏ hàng.`);
-}
-
-// Thêm sản phẩm vào giao diện
-products.forEach(product => {
-    addProductToUI(product);
+$(function () {
+    App.init();
 });
+var App = {
+    init: function () {
+        this.datetime(), this.side.nav(), this.search.bar(), this.navigation(), this.hyperlinks(), setInterval("App.datetime();", 1e3)
+    },
+    datetime: function () {
+        var e = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"),
+            t = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
+            a = new Date,
+            i = a.getYear();
+        1e3 > i && (i += 1900);
+        var s = a.getDay(),
+            n = a.getMonth(),
+            r = a.getDate();
+        10 > r && (r = "0" + r);
+        var l = a.getHours(),
+            c = a.getMinutes(),
+            h = a.getSeconds(),
+            o = "AM";
+        l >= 12 && (o = "PM"), l > 12 && (l -= 12), 0 == l && (l = 12), 9 >= c && (c = "0" + c), 9 >= h && (h = "0" + h), $(".welcome .datetime .day").text(e[s]), $(".welcome .datetime .date").text(t[n] + " " + r + ", " + i), $(".welcome .datetime .time").text(l + ":" + c + ":" + h + " " + o)
+    },
+    title: function (e) {
+        return $(".header>.title").text(e)
+    },
+    side: {
+        nav: function () {
+            this.toggle(), this.navigation()
+        },
+        toggle: function () {
+            $(".ion-ios-navicon").on("touchstart click", function (e) {
+                e.preventDefault(), $(".sidebar").toggleClass("active"), $(".nav").removeClass("active"), $(".sidebar .sidebar-overlay").removeClass("fadeOut animated").addClass("fadeIn animated")
+            }), $(".sidebar .sidebar-overlay").on("touchstart click", function (e) {
+                e.preventDefault(), $(".ion-ios-navicon").click(), $(this).removeClass("fadeIn").addClass("fadeOut")
+            })
+        },
+        navigation: function () {
+            $(".nav-left a").on("touchstart click", function (e) {
+                e.preventDefault();
+                var t = $(this).attr("href").replace("#", "");
+                $(".sidebar").toggleClass("active"), $(".html").removeClass("visible"), "home" == t || "" == t || null == t ? $(".html.welcome").addClass("visible") : $(".html." + t).addClass("visible"), App.title($(this).text())
+            })
+        }
+    },
+    search: {
+        bar: function () {
+            $(".header .ion-ios-search").on("touchstart click", function () {
+                var e = ($(".header .search input").hasClass("search-visible"), $(".header .search input").val());
+                return "" != e && null != e ? (App.search.html($(".header .search input").val()), !1) : ($(".nav").removeClass("active"), $(".header .search input").focus(), void $(".header .search input").toggleClass("search-visible"))
+            }), $(".search form").on("submit", function (e) {
+                e.preventDefault(), App.search.html($(".header .search input").val())
+            })
+        },
+        html: function (e) {
+            $(".search input").removeClass("search-visible"), $(".html").removeClass("visible"), $(".html.search").addClass("visible"), App.title("Result"), $(".html.search").html($(".html.search").html()), $(".html.search .key").html(e), $(".header .search input").val("")
+        }
+    },
+    navigation: function () {
+        $(".nav .mask").on("touchstart click", function (e) {
+            e.preventDefault(), $(this).parent().toggleClass("active")
+        })
+    },
+    hyperlinks: function () {
+        $(".nav .nav-item").on("click", function (e) {
+            e.preventDefault();
+            var t = $(this).attr("href").replace("#", "");
+            $(".html").removeClass("visible"), $(".html." + t).addClass("visible"), $(".nav").toggleClass("active"), App.title($(this).text())
+        })
+    }
+};
